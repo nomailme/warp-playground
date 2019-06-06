ea031084098363861354cc2f955e71356de2e569
Iskander Yarmukhametov <iskander.yarmukhametov@infotecs.ru>
2019-06-06 11:15:32 +0300

    Remove image after build

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
-docker build . -t warp-playground
+docker build . -t warp-playground:latest
 $location = Get-Location
-docker run -v $location`:`/app/result warp-playground /bin/cp -r /app/out /app/result
+docker run -v $location`:`/app/result warp-playground:latest /bin/cp -r /app/out /app/result
+docker rmi warp-playground:latest
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build . -t warp-playground
-docker run -v `pwd`:/app/result warp-playground /bin/cp -r /app/out /app/result
+docker build . -t warp-playground:latest
+docker run -v `pwd`:/app/result warp-playground:latest /bin/cp -r /app/out /app/result
+docker rmi warp-playground:latest
